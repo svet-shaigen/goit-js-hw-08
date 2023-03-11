9f529c022bf69c4289ba96f7a9ad5df03b31f769
svet-shaigen <110323981+svet-shaigen@users.noreply.github.com>
2023-03-11 17:57:48 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,4 +4,29 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryItemsMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `<li>
+              <a class="gallery__item" href="${original}">
+               <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+               />
+             </a>
+           </li>`
+  }).join('');
+}
+
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+const modalLightbox = new SimpleLightbox('.gallery .gallery__item', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  
+
+
