dd3bbe93edad2461531d96f2e452dc09443bda21
svet-shaigen <kuzovasvitlana@gmail.com>
2023-03-13 13:09:22 +0000

    Deploying to gh-pages from @ svet-shaigen/goit-js-hw-08@0782b07a8a83fee9661c52c6ab01b4e6da4e362d 🚀

---
+++ 03-feedback.5bb87211.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",(function(e){}));
+//# sourceMappingURL=03-feedback.5bb87211.js.map
---
+++ 03-feedback.e951bf43.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",(function(e){}));
+//# sourceMappingURL=03-feedback.e951bf43.js.map
