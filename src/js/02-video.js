0782b07a8a83fee9661c52c6ab01b4e6da4e362d
svet-shaigen <110323981+svet-shaigen@users.noreply.github.com>
2023-03-13 15:08:56 +0200

    Add changed files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
 
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    const CURRENT_TIME_KEY = "videoplayer-current-time";
+
+    player.on('timeupdate', throttle(saveTime, 1000));
+
+function saveTime(event) {
+  let seconds = event.seconds;
+  localStorage.setItem(CURRENT_TIME_KEY, seconds);
+}
+
+const currentVideoTime = localStorage.getItem(CURRENT_TIME_KEY);
+if (currentVideoTime) {
+  player.setCurrentTime(currentVideoTime);
+}
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,10 @@
+const feedbackForm = document.querySelector('.feedback-form');
 
+feedbackForm.addEventListener("input", feedbackLocallStorage);
+
+function feedbackLocallStorage (event) {
+  
+}
+
+
+const FEEDBACK_FORM_KEY = "feedback-form-state";
