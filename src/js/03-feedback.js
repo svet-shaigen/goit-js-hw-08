2181b7c0db87b56d3082cba78fd5cdfb1cfa9121
svet-shaigen <110323981+svet-shaigen@users.noreply.github.com>
2023-03-14 18:58:12 +0200

    Add changed file

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,36 @@
+import throttle from "lodash.throttle";
+
 const feedbackForm = document.querySelector('.feedback-form');
 
-feedbackForm.addEventListener("input", feedbackLocallStorage);
+feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener("input", throttle(onInputData, 500));
 
-function feedbackLocallStorage (event) {
+let formData = {};
+
+const KEY_FEEDBACK_FORM = "feedback-form-state";
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const {
+      elements: { email, message }
+    } = event.currentTarget;
   
+    if (email.value === "" || message.value === "") {
+      return alert("Увага! Всі поля мають бути заповненні!");
+    }
+
+  event.currentTarget.reset();
+  localStorage.removeItem(KEY_FEEDBACK_FORM);
+  console.log(formData);
+  // formData = {};
+}
+ 
+
+function onInputData(event) {
+    formData[event.target.name] = event.target.value;
+  const formDataJson = JSON.stringify(formData);
+  localStorage.setItem(KEY_FEEDBACK_FORM, formDataJson);
 }
 
 
-const FEEDBACK_FORM_KEY = "feedback-form-state";
+
